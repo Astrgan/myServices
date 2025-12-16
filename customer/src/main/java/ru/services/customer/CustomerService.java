e4432a387bace7de007ab621d4543de0ba19b76a
rost0 <astrgan@gmail.com>
2025-12-16 23:01:13 +1000

    add RMQ final

--- customer/src/main/java/ru/services/customer/CustomerService.java
+++ customer/src/main/java/ru/services/customer/CustomerService.java
@@ -1,24 +1,19 @@
 package ru.services.customer;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import ru.vasilev.amqp.RabbitMQMessageProducer;
 import ru.vasilev.clients.fraud.FraudCheckResponse;
 import ru.vasilev.clients.fraud.FraudClient;
-import ru.vasilev.clients.notificationClient.NotificationClient;
 import ru.vasilev.clients.notificationClient.NotificationRequest;
 
-@Slf4j
 @Service
 @AllArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
-    private final RestTemplate restTemplate;
     private final FraudClient fraudClient;
-    private final NotificationClient notificationClient;
+    private final RabbitMQMessageProducer rabbitMQMessageProducer;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -26,29 +21,28 @@ public class CustomerService {
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
+        // todo: check if email valid
+        // todo: check if email not taken
         customerRepository.saveAndFlush(customer);
 
-/*        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://FRAUD/api/v1/fraud-check/{customerId}",
-                FraudCheckResponse.class, customer.getId()  );
- */
-        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
+        FraudCheckResponse fraudCheckResponse =
+                fraudClient.isFraudster(customer.getId());
 
-        if (fraudCheckResponse.isFraudster())
+        if (fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("fraudster");
+        }
 
-        log.info("registerCustomer");
-        log.info("traceId from MDC = {}", MDC.get("traceId"));
-        log.info("spanId from MDC = {}", MDC.get("spanId"));
-
-        // todo: make it async. i.e add to queue
-        notificationClient.sendNotification(
-                new NotificationRequest(
-                        customer.getId(),
-                        customer.getEmail(),
-                        String.format("Hi %s, welcome to Amigoscode...",
-                                customer.getFirstName())
-                )
+        NotificationRequest notificationRequest = new NotificationRequest(
+                customer.getId(),
+                customer.getEmail(),
+                String.format("Hi %s, welcome to Amigoscode...",
+                        customer.getFirstName())
         );
+        rabbitMQMessageProducer.publish(
+                notificationRequest,
+                "internal.exchange",
+                "internal.notification.routing-key"
+        );
+
     }
 }
